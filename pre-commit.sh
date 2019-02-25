713cfeb4ddf6d59c29471b25aa7e29a9cdd505b3
Scott Rogener <srogener@gmail.com>
2019-02-25 00:51:31 -0500

    more pre commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-echo "pre-commit start"
 # pre-commit.sh
 STASH_NAME="pre-commit-$(date +%s)"
 git stash save -q --keep-index $STASH_NAME
@@ -15,6 +14,5 @@ if [[ $STASHES == "$STASH_NAME" ]]; then
   git stash pop -q
 fi
 
-echo "pre-commit complete"
 [ $RESULT -ne 0 ] && exit 1
 exit 0
